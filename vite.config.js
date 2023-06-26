1afd9cdf357a0e30cc131f19fb1074bf83d628a0
kellbot <kellbot@gmail.com>
2023-06-26 14:43:15 -0400

    Missing trailing /

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+  base: '/mcp-home/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
