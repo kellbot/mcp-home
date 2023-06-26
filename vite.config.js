4d9188b323d8b23ceaefe013285b6085303799e0
kellbot <kellbot@gmail.com>
2023-06-26 14:50:31 -0400

    arbitrary changes

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  base: '/mcp-home/',
+  base: './',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
