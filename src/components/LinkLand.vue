20b9587bcd7c1ddfcdcfd8d7b3d086a2b4b70b87
kellbot <kellbot@gmail.com>
2023-06-26 13:43:56 -0400

    Initial commit

---
+++ src/components/LinkLand.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card     class="mx-auto pa-2"
+    max-width="400">
+      <v-list>
+        <v-list-subheader>SERVICES</v-list-subheader>
+      <v-list-item
+        v-for="item, i in items"
+        :key="i"
+        :title="item.title"
+        :subtitle="item.subtitle"
+        :value="item.link"
+        color="primary"
+        @click="go(item.link)"
+      ><template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+    </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script setup>
+  function go(link) {
+    window.location.href = link;
+  }
+
+  let items = [
+  {
+      title: "Logitech Media Server",
+      subtitle: "Music",
+      icon: 'mdi-music',
+      link: 'http://mcp:9000/'
+    },
+    {
+      title: "Sonarr",
+      subtitle: "TV Shows",
+      icon: 'mdi-television',
+      link: 'http://mcp:38989/'
+    },
+    {
+      title: "Radarr",
+      subtitle: "Movies",
+      icon: 'mdi-movie',  
+          link: 'http://mcp:7878/'
+    },
+    {
+      title: "QBittorrent",
+      subtitle: "Torrent Downloads",
+      icon: "mdi-download",
+      link: 'http://mcp:8080/'
+    },
+    {
+      title: "Overseerr",
+      subtitle: "Requests",
+      icon: 'mdi-plus',
+      link: 'http://mcp:5055/'
+    }
+  ]
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <LinkLand />
+</template>
+
+<script setup>
+  import LinkLand from '@/components/LinkLand.vue'
+</script>
